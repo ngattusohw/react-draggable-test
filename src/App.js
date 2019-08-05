d12607ab683cba5d3710a9927e8216c75504464e
ngattusohw <ngattusohw@gmail.com>
2019-08-04 22:21:11 -0400

    First droppable list with reordering and styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const getItems = count =>
+	Array.from({ length: count }, (v, k) => k).map(k => ({
+		id: `item-${k}`,
+		content: `item ${k}`,
+	}));
 
 const App = () => {
+	const [items, setItems] = useState([]);
+	const [destinationItems, setDestinationItems] = useState([]);
+
+	const grid = 8;
+
+	useEffect(() => {
+		setItems(getItems(5));
+	}, []);
+
+	const getListStyle = isDraggingOver => ({
+		background: isDraggingOver ? 'lightblue' : 'lightgrey',
+		display: 'flex',
+		padding: 8,
+		overflow: 'auto',
+	});
+
+	const onDragEnd = result => {
+		const _items = reorder(items, result.source.index, result.destination.index);
+
+		setItems(_items);
+		console.log(result);
+	};
+
+	const reorder = (list, startIndex, endIndex) => {
+		const result = Array.from(list);
+		const [removed] = result.splice(startIndex, 1);
+		result.splice(endIndex, 0, removed);
+
+		return result;
+	};
+
+	const getItemStyle = (isDragging, draggableStyle) => ({
+		// some basic styles to make the items look a bit nicer
+		userSelect: 'none',
+		padding: grid * 2,
+		margin: `0 ${grid}px 0 0`,
+
+		// change background colour if dragging
+		background: isDragging ? 'lightgreen' : 'grey',
+
+		// styles we need to apply on draggables
+		...draggableStyle,
+	});
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Droppable direction="horizontal" droppableId="list1">
+					{(provided, snapshot) => (
+						<div
+							ref={provided.innerRef}
+							style={getListStyle(snapshot.isDraggingOver)}
+							{...provided.droppableProps}
+						>
+							{items.map((item, index) => (
+								<Draggable key={item.id} draggableId={item.id} index={index}>
+									{(provided, snapshot) => (
+										<div
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+											style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+										>
+											{item.content}
+										</div>
+									)}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+				{/* <Droppable direction="horizontal" droppableId="list2">
+					<div
+						ref={provided.innerRef}
+						style={getListStyle(snapshot.isDraggingOver)}
+						{...provided.droppableProps}
+					>
+						{items.map((item, index) => (
+							<Draggable key={item.id} draggableId={item.id} index={index}>
+								{(provided, snapshot) => (
+									<div
+										ref={provided.innerRef}
+										{...provided.draggableProps}
+										{...provided.dragHandleProps}
+										style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+									>
+										{item.content}
+									</div>
+								)}
+							</Draggable>
+						))}
+						{provided.placeholder}
+					</div>
+					)}
+				</Droppable> */}
+			</DragDropContext>
 		</div>
 	);
 };
